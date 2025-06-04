162b737926a46c55e88029286a081ebc3062ce58
Robert Goené <robert@goene.nl>
2025-06-04 09:12:08 +0200

    Update observablehq.config.js

--- observablehq.config.js
+++ observablehq.config.js
@@ -48,7 +48,7 @@ export default {
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
   // pager: true, // whether to show previous & next links in the footer
-  output: "docs", // path to the output root for build
+  output: "dist", // path to the output root for build
   search: true,
   // linkify: true, // convert URLs in Markdown to links
   // typographer: false, // smart quotes and other typographic improvements
